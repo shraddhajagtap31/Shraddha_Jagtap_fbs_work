506b91e90ddcefc6a7df51c875bef1779fbe5ce9
shraddhajagtap31 <jagtapshraddha31@gmail.com>
2026-07-20 11:12:38 +0530

    Add files via upload

---
+++ firstbit/Assignments/Assignment 0/Question3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(){
+	float celsius, fahrenheit;
+	celsius = 22.5;
+	fahrenheit = (9.0 / 5.0) * celsius + 32;
+	printf("Temperature in Fahrenheit = %f", fahrenheit);
+}
---
+++ firstbit/Assignments/Assignment 0/Question9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+	int base,height,area;
+	base = 8;
+	height = 6;
+	area = 0.5 * base * height;
+	printf("the area of triangle is %d",area);
+}
+
+	
