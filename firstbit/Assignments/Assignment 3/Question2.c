374105a0ce03139c89c1fdbf6cc395e299ddfff5
shraddhajagtap31 <jagtapshraddha31@gmail.com>
2026-08-27 21:40:05 +0530

    Update multiplication program to take user input
    Modified the program to accept user input for multiplication.

--- firstbit/Assignments/Assignment 3/Question2.c
+++ firstbit/Assignments/Assignment 3/Question2.c
@@ -2,10 +2,13 @@
 
 void main(){
 	int i = 1;
-	int n =0;
-	while(i <= 10){
-		n = i*5;
-		printf(" %d",n);
-		i++;
-	}
+	int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    while(i <= 10)
+    {
+        printf("%d ", n * i);
+        i++;
+    }
 }
